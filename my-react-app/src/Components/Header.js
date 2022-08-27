542d8358a4ac6a159055ee8b37dce26d68b5e5fb
Geod11 <danilescugeorge@yahoo.com>
2022-08-27 12:48:38 +0200

    React app 27 Aug

---
+++ my-react-app/src/Components/Header.js
@@ -0,0 +1,16 @@
+import '../Style_css/Header.scss'
+
+function Header () {
+return (
+<div className="Header-main-container">
+<ul className="Header-main-list">
+<li className="Header-child-list">Home</li>
+<li className="Header-child-list">About</li>
+<li className="Header-child-list">Projects</li>
+<li className="Header-child-list">Skills</li>
+<li className="Header-child-list">Contact</li>
+</ul>
+</div>
+)}
+
+export default Header
